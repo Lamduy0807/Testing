e782177f58e508fb716b6317f91aa553afb960e2
Lamduy0807 <19521421@gm.uit.edu.vn>
2021-10-17 20:44:06 +0700

    add file

---
+++ Function1Testing/Function1Testing/CheckDay.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Function1Testing
+{
+    public class CheckDay
+    {
+        public int CheckDayinMonth(int month, int year)
+        {
+            if (month >= 1 && month <= 12)
+            {
+                switch (month)
+                {
+                    case 1: return 31;
+                    case 3: return 31;
+                    case 5: return 31;
+                    case 7: return 31;
+                    case 8: return 31;
+                    case 10: return 31;
+                    case 12: return 31;
+                    case 4: return 30;
+                    case 6: return 30;
+                    case 9: return 30;
+                    case 11: return 30;
+
+                    case 2:
+                        {
+                            if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0))    // nam nhuan
+                                return 29;
+                            else
+                                return 28;
+                        }
+                }
+            }
+            return 0;
+        }
+    }
+}
---
+++ Function1Testing/UnittestFunction1/Test.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using NUnit.Framework;
+using Function1Testing;
+namespace UnittestFunction1
+{
+    [TestFixture]
+    public class TestCase
+    {
+        [TestCase(1,2000,31)]
+        [TestCase(3, 2000, 31)]
+        [TestCase(5, 2000, 31)]
+        [TestCase(7, 2000, 31)]
+        [TestCase(8, 2000, 31)]
+        [TestCase(10, 2000, 31)]
+        [TestCase(12, 2000, 31)]
+        [TestCase(1, 2000, 31)]
+        [TestCase(4, 2000, 30)]
+        [TestCase(6, 2000, 30)]
+        [TestCase(9, 2000, 30)]
+        [TestCase(11, 2000, 30)]
+        [TestCase(0, 2000, 0)]
+        [TestCase(2, 2000, 29)]
+        [TestCase(2, 1900, 28)]
+        [TestCase(2, 1904, 29)]
+        [TestCase(2, 2009, 28)]
+
+        public void Test_day_in_month(int month, int year, int result)
+        {
+            var check = new CheckDay();
+            Assert.AreEqual(result, check.CheckDayinMonth(month, year));
+        }
+    }
+}
